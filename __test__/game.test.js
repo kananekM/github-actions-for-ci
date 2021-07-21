919f9aa2c05633fd32b79ca83f66b761c99f4c96
kananek mongkolsawat <toptoppy@odds.team>
2021-07-21 22:05:21 +0700

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,11 +1,11 @@
 const Game = require('../src/game').default
-// const fs = require('fs')
+const fs = require('fs')
 
 describe('App', () => {
-  //   it('Contains the compiled JavaScript', async () => {
-  //     const data = fs.readFileSync('./public/main.js', 'utf8')
-  //     expect(data).toMatchSnapshot()
-  //   })
+    it('Contains the compiled JavaScript', async () => {
+      const data = fs.readFileSync('./public/main.js', 'utf8')
+      expect(data).toMatchSnapshot()
+    })
 })
 
 describe('Game', () => {
@@ -17,10 +17,10 @@ describe('Game', () => {
   })
 
   describe('Game', () => {
-  //     it('Initializes with two players', async () => {
-  //       expect(game.p1).toBe('Salem')
-  //       expect(game.p2).toBe('Nate')
-  //     })
+      it('Initializes with two players', async () => {
+        expect(game.p1).toBe('Salem')
+        expect(game.p2).toBe('Nate')
+      })
 
     it('Initializes with an empty board', async () => {
       for (let r = 0; r < game.board.length; r++) {
@@ -29,12 +29,12 @@ describe('Game', () => {
         }
       }
     })
-  //     it('Starts the game with a random player', async () => {
-  //        Math.random = () => 0.4
-  //        expect(new Game(p1, p2).player).toBe('Salem')
-  //        Math.random = () => 0.6
-  //        expect(new Game(p1, p2).player).toBe('Nate')
-  //     })
+      it('Starts the game with a random player', async () => {
+         Math.random = () => 0.4
+         expect(new Game(p1, p2).player).toBe('Salem')
+         Math.random = () => 0.6
+         expect(new Game(p1, p2).player).toBe('Nate')
+      })
   })
 
   describe('turn', () => {
@@ -50,13 +50,13 @@ describe('Game', () => {
   })
 
   describe('nextPlayer', () => {
-  //     it('Sets the current player to be whoever it is not', async () => {
-  //       Math.random = () => 0.4
-  //       const game = new Game(p1, p2)
-  //       expect(game.player).toBe('Salem')
-  //       game.nextPlayer()
-  //       expect(game.player).toBe('Nate')
-  //     })
+      it('Sets the current player to be whoever it is not', async () => {
+        Math.random = () => 0.4
+        const game = new Game(p1, p2)
+        expect(game.player).toBe('Salem')
+        game.nextPlayer()
+        expect(game.player).toBe('Nate')
+      })
   })
 
   describe('hasWinner', () => {
